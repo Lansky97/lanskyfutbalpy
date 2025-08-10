208aa21f3576b3ffd320ddf61fc538c15580c1f4
Lansky97 <75474764+Lansky97@users.noreply.github.com>
2025-08-10 09:52:30 +0100

    Simulation methods to aggregate simulations

--- simulation.py
+++ simulation.py
@@ -1,11 +1,15 @@
 import copy
 from match import Match, SimmedMatch
+import pandas as pd
+import numpy as np
 
 class Simulation:
     def __init__(self,league, n_trials=1000):
         self.league = league
         self.n_trials = n_trials
         self.simmed_leagues = self.simulate_season()
+        self.mean_final_table = self.mean_final_table()
+        self.position_odds = self.position_odds()
 
     def simulate_season(self):
         simmed_leagues = []
@@ -13,7 +17,7 @@ class Simulation:
         for _ in range(self.n_trials):
             sim_league = copy.deepcopy(self.league)
             unique_dates = sim_league.fixtures['Date'].unique()
-            for idx, match_date in enumerate(unique_dates):
+            for match_date in unique_dates:
                 md_fixtures = sim_league.fixtures[sim_league.fixtures['Date'] == match_date]
                 md_results = []
                 day_matches = SimmedMatch.from_fixtures(sim_league.teams, md_fixtures, sim_league.xG_factor)
@@ -24,3 +28,22 @@ class Simulation:
             sim_league.league_table = sim_league.generate_league_table()
             simmed_leagues.append(sim_league)
         return simmed_leagues
+
+    def mean_final_table(self):
+       
+        tables = [sim.league_table for sim in self.simmed_leagues]
+        all_tables = pd.concat(tables)
+        mean_table = all_tables.groupby('Team', as_index=False).mean()
+        mean_table = mean_table.sort_values(by=(['Points', 'Goal Difference', 'Goals']), ascending=False)
+        mean_table['Pos'] = range(1, len(mean_table) + 1) 
+        return mean_table
+
+    def position_odds(self):
+        tables = [sim.league_table for sim in self.simmed_leagues]
+        all_tables = pd.concat(tables)
+        all_tables['Pos'] = all_tables['Pos'].astype(int) 
+
+        pos_counts = all_tables.groupby('Team')['Pos'].value_counts().unstack(fill_value=0)
+        probs = pos_counts / self.n_trials
+
+        return probs
